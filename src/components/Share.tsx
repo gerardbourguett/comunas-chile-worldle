e61a5d11f235adcbf7a1c5d60f79693047e2cf0a
Gerard <gerard.bourguett@gmail.com>
2022-03-15 23:44:51 -0300

    cambio link

--- src/components/Share.tsx
+++ src/components/Share.tsx
@@ -49,7 +49,7 @@ export function Share({
     const bestPercent = `(${computeProximityPercent(
       bestDistance
     ).toString()}%)`;
-    const title = `#Worldle #${dayCount} ${guessCount}/6 ${bestPercent}${difficultyModifierEmoji}`;
+    const title = `#Comunas de Chile #${dayCount} ${guessCount}/6 ${bestPercent}${difficultyModifierEmoji}`;
 
     const guessString = guesses
       .map((guess) => {
